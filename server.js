5c1509def97a7112c3332164440424d613ac5d05
Anthony Gonzales <anthonygonzales79072@gmail.com>
2021-10-26 15:21:58 -0500

    Initial Setup

---
+++ server.js
@@ -0,0 +1,18 @@
+/////////////////////////
+// import deps
+////////////////////////
+const express = require("express")
+
+
+// app object
+const app = express()
+
+
+// route
+app.get("/", (req, res) => {
+    res.send("This App is working")
+})
+
+// listener
+const PORT = process.env.PORT || 3000
+app.listen(PORT, console.log(`listening on port ${PORT}`))
